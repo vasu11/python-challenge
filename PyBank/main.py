b9348d6160c2bb9fa6b7a857283f27ede77b33a7
vasu11 <vasu01@gmail.com>
2021-04-07 08:21:22 -0700

    Add files via upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,3 +2,19 @@ import os
 
 import csv
 
+csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row)
