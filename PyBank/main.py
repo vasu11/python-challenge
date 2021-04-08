1caa0ceb9a30ec6c376a0676e346070e37f48fd0
vasu11 <vasu01@gmail.com>
2021-04-07 20:23:48 -0700

    main.py added increase and decrease info

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,6 +6,8 @@ csvpath = os.path.join('', 'Resources', 'budget_data.csv')
 
 count = 0
 net_total = 0
+greatest_increase = 0
+greatest_decrease = 0
 
 with open(csvpath) as csvfile:
 
@@ -22,5 +24,16 @@ with open(csvpath) as csvfile:
     for row in csvreader:
         count = count + 1
         net_total = net_total + int(row[1])
+        if int(row[1]) > greatest_increase:
+            greatest_increase = int(row[1])
+            greatest_increase_date = row[0]
 
+        if int(row[1]) < greatest_decrease:
+            greatest_decrease = int(row[1])
+            greatest_decrease_date = row[0]
+
+print ("total months: " + str(count))
 print ("net total: " + str(net_total))
+print ("greatest increase in profits: " + greatest_increase_date + " " + str(greatest_increase))
+print ("Greatest decrease in profits: " + greatest_decrease_date + " " + str(greatest_decrease))
+
